88e3ff8da975d12462c083c298c621526928c6d3
bradybellini <bellinibrady65@gmail.com>
2020-01-09 14:58:16 -0700

    testing still

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from tkinter import *
 import requests
+import json
+
+
 
 
 app = Tk()
 app.title('test')
 app.geometry('700x350')
+label1 = Label(app, text='Hello')
+label2 = Label(app, text='Hello00')
+
+label1.grid(row=0, column=0)
+label2.grid(row=1, column=0)
 
 app.mainloop()
