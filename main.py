5e7c1cc41bcedacfc0cb96edf149a3b7b50cf7b8
bradybellini <bellinibrady65@gmail.com>
2020-01-10 23:15:25 -0700

    trying to not use grid

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import ttk, font
 from PIL import Image, ImageTk
 import requests
 import json
+import feedparser
 import time
 import datetime
 import sys
@@ -25,15 +26,23 @@ app.title('Welcome')
 # app.attributes('-fullscreen', True)
 
 app.geometry('1600x900')
-app.configure(background='black')
+app.configure(background='#1c2431')
 
 
 app.iconbitmap('rocket.ico')
 clock_font = font.Font(family='Helvetica', size=20, weight='bold')
 headline_font = font.Font(family='Helvetia', size=12, weight='bold')
 
+img_1 = ImageTk.PhotoImage(Image.open("comfy.png"))
+img_label = Label(image=img_1, borderwidth=0)
+img_label.place(x=250, y=126)
 
-img_1 = ImageTk.PhotoImage(Image.open(""))
+def headlines():
+    thirty_min = 1800000
+    f = feedparser.parse('https://api.axios.com/feed/top/')
+    headline_1_label = Label(app, text=f.entries[0].title, wraplength=400, font=headline_font, background='#1c2431', fg='white', anchor='w').place(x=800, y=300)
+
+    app.after(thirty_min, headlines)
 
 
 # def reddit_all():
@@ -55,16 +64,18 @@ def clock():
     date = datetime.datetime.now().strftime('%A %d %B %Y')
     app.after(1000, clock)
 
-    date_label = Label(app,text=date, font=clock_font).grid(row=1, column=0)
-    clock_label = Label(app, text=time, font=clock_font).grid(row=0, column=0)
+    date_label = Label(app,text=date, font=clock_font).place(x=1050, y=200)
+
+    clock_label = Label(app, text=time, font=clock_font).place(x=1050, y=300)
 
 if __name__ == "__main__":
     clock()
-    reddit_all()
+    headlines()
 app.mainloop()
 
     
-
+# .grid(row=1, column=0)
+# .grid(row=0, column=0)
 
 # picture = ImageTk.PhotoImage(Image.open('jungle.png'))
 # label3 = Label(image=picture)
